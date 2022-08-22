d71395bd3ed040bbeb681aedd675149b826c916a
Tom <synapsenkoch27@gmail.com>
2022-08-22 21:04:10 +0200

    changes

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "lambda_for_gifs" {
-  source = "./lambda"
-  function_name = "lambda-display-gif"
-  lambda_role = "lambda-for-gif-role"
-  lambda_policy = "lambda-for-gif-policy"
-  environment = "dev"
-  region = var.region
-  accountId = var.accountId
-}
